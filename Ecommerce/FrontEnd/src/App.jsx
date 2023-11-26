d1f046038c4a422ba95479f7428621b5852484d4
Sujith <p.sujithk@gmail.com>
2023-11-26 23:31:19 +0530

    Items added

--- Ecommerce/FrontEnd/src/App.jsx
+++ Ecommerce/FrontEnd/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import './App.css'
-import Navbar from './Components/Navbar/Navbar'
 import Shop from './Pages/Shop'
 import ShopCategory from './Pages/ShopCategory'
 import Product from './Pages/Product'
 import LoginSignup from './Pages/LoginSignup'
 import Cart from './Pages/Cart'
-import Hero from './Components/Hero/Hero'
 
 
 function App() {
@@ -16,8 +14,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
-        <Hero />
+        
         <Routes>
           <Route path='/' element={<Shop/>}/>
           <Route path='/men' element={<ShopCategory category="men"/>}/>
--- Ecommerce/FrontEnd/src/Pages/Shop.jsx
+++ Ecommerce/FrontEnd/src/Pages/Shop.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import Hero from '../Components/Hero/Hero';
-
+import React from "react";
+import Hero from "../Components/Hero/Hero";
+import Popular from "../Components/Popular/Popular";
+import Navbar from "../Components/Navbar/Navbar";
+import Offers from "../Components/Offers/Offers";
 
 const Shop = () => {
-return (
-  <>
-  </>
-  )
-} 
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Popular />
+      <Offers />
+    </>
+  );
+};
 
 export default Shop;
